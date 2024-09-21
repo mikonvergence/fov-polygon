dadf5f508d5be8f1b2ea906e3cfa496ca2491cf7
mikonvergence <Mczerkawski96@gmail.com>
2024-09-21 19:09:56 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='geobuffers',
+    version='0.1',
+    description='geobuffers',
+    author='Mikolaj Czerkawski',
+    install_requires=["shapely","pyproj"]
+)
