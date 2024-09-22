7f1fbef82812a8919fd61a31dcef32cafc4b39db
mikonvergence <Mczerkawski96@gmail.com>
2024-09-22 14:09:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     description='geobuffers',
     long_description='geobuffers mini package for calculating geodetic areas around points',
     author='Mikolaj Czerkawski',
+    author_email="mczerkawski96@gmail.com",
+    package_dir={"geobuffers":"src"},
     install_requires=["shapely","pyproj"]
 )
